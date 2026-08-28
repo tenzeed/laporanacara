1dc4ef5c7290aa55668e0e3d5ce2749595978e2f
Abdul-mukhyididn <abdulmukhyidin.19@gmail.com>
2026-08-28 18:15:27 +0700

    matikan zoomin

--- app/layout.tsx
+++ app/layout.tsx
@@ -38,6 +38,8 @@ export const metadata: Metadata = {
 export const viewport: Viewport = {
   width: "device-width",
   initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
   themeColor: "#1F6F5C",
 };
 
